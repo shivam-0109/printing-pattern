66a051c246b170b534358d43f25cf5c63ed76972
shivam kumar singh <89032612+shivam01091@users.noreply.github.com>
2022-05-24 06:27:44 +0530

    Create code.c

---
+++ code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>  
+#include <conio.h> 
+#include <limits.h> 
+int main()  
+{  
+    
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+        printf("%d",n);
+    }
+    printf("\n");
+        }
+
+    return 0;
+}  
